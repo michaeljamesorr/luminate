bae66dfb6f9f754375c3cb84626349914bf06224
Michael Orr <michaeljamesorr@gmail.com>
2020-04-10 23:15:54 +1200

    FPS display

--- waves.py
+++ waves.py
@@ -5,23 +5,25 @@ import random
 
 class MainApp(pyglet.window.Window):
 
+	displayFPS = True
+
 	def __init__(self, width, height):
 		pyglet.window.Window.__init__(self, width=width, height=height)
 		self.numPoints = 5000
 		random.seed(0)
 
-	def on_mouse_press(self, x, y, button, modifiers):
-		#print(str(x)+","+str(y))
-		pass
-
-	def on_mouse_motion(self, x, y, dx, dy):
-		pass
+		if self.displayFPS:
+			self.fps_display = pyglet.window.FPSDisplay(self)
 
 	def on_draw(self):
 		self.clear()
+		if self.displayFPS:
+			self.fps_display.draw()
+
 		points = random_points(self.numPoints, self.width, self.height)
 		colours = random_colours(self.numPoints)
-		pyglet.graphics.draw(self.numPoints, pyglet.gl.GL_POINTS, ('v2i', points), ('c3B', colours))
+		pyglet.graphics.draw(self.numPoints, pyglet.gl.GL_POINTS,
+		 ('v2i/stream', points), ('c3B/stream', colours))
 
 def random_points(count, xMax, yMax):
 	points = []
@@ -33,9 +35,12 @@ def random_points(count, xMax, yMax):
 def random_colours(count):
 	return bytearray(random.getrandbits(8) for _ in range(count*3))
 
+def update(dt):
+	pass
 
 def main():
 	MainApp(width=1280, height=720)
+	pyglet.clock.schedule_interval(update, 0.01)
 	pyglet.app.run()
 
 
