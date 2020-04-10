2c41c35545dfce128b2ddc7a8e6932068406e17d
Michael Orr <michaeljamesorr@gmail.com>
2020-04-10 22:44:39 +1200

    random coloured noise

--- waves.py
+++ waves.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python3
 
 import pyglet
+import random
 
-window = pyglet.window.Window()
+class MainApp(pyglet.window.Window):
 
-label = pyglet.text.Label('Hello world', x=window.width//2, y=window.height//2)
+	def __init__(self, width, height):
+		pyglet.window.Window.__init__(self, width=width, height=height)
+		self.numPoints = 5000
+		random.seed(0)
+
+	def on_mouse_press(self, x, y, button, modifiers):
+		#print(str(x)+","+str(y))
+		pass
+
+	def on_mouse_motion(self, x, y, dx, dy):
+		pass
+
+	def on_draw(self):
+		self.clear()
+		points = random_points(self.numPoints, self.width, self.height)
+		colours = random_colours(self.numPoints)
+		pyglet.graphics.draw(self.numPoints, pyglet.gl.GL_POINTS, ('v2i', points), ('c3B', colours))
+
+def random_points(count, xMax, yMax):
+	points = []
+	for _ in range(count):
+		points.append(random.randint(0, xMax))
+		points.append(random.randint(0, yMax))
+	return points
+
+def random_colours(count):
+	return bytearray(random.getrandbits(8) for _ in range(count*3))
+
 
 def main():
+	MainApp(width=1280, height=720)
 	pyglet.app.run()
-	
-
-@window.event
-def on_draw():
-
-	window.clear()
-	label.draw()
-
-	pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', (10, 15, 30, 35)))
 
 
 
