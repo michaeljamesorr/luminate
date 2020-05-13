fe6c2eb46b65a3b581ae9d49d1ea16d5161a7509
Michael Orr <michaeljamesorr@gmail.com>
2020-05-13 23:00:41 +1200

    rename package to luminate

--- tests/utility_test.py
+++ tests/utility_test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-import waves.utility
+import luminate.utility
 
 
 class TestUtilityFunctions(unittest.TestCase):
@@ -10,14 +10,14 @@ class TestUtilityFunctions(unittest.TestCase):
         count = 10
         x_max = 100
         y_max = 200
-        points = waves.utility.random_points(count, x_max, y_max)
+        points = luminate.utility.random_points(count, x_max, y_max)
 
         self.assertTrue(isinstance(points, np.ndarray))
         self.assertEqual(points.shape, (2, count))
 
     def test_random_colours(self):
         count = 10
-        colours = waves.utility.random_colours(count)
+        colours = luminate.utility.random_colours(count)
 
         self.assertTrue(isinstance(colours, bytes))
         self.assertEqual(len(colours), count*3)
